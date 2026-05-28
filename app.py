03be62fac672e856b1abd79f8c224131e5b12f48
OGAM123 <ogam1232011@gmail.com>
2026-05-28 15:06:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ mis_objetivos = {
 # Crea un diccionario para "traducir" los nombres en la pantalla
 nombres_pantalla = {
     "EGEPEC1.LM": "ORYGENC1"
-    "BTC-USD": "Bitcoin"
-  }
+      }
 
 # Luego, cuando imprimas el nombre en Streamlit, usa:
 # st.write(f"Acción: {nombres_pantalla.get(ticker, ticker)}")
